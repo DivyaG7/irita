4df6ebc0e346c2908c3ba112b519766921dd2fdb
unknown <divyaneela75@gmail.com>
2024-03-13 16:48:32 +0530

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '../pages/About.css'
+import aboutImg from '../assets/About-img.png'
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="container mt-5 pt-4">
+      <div className='About'>
+        <h1 className="mb-5">About IRITA</h1>
+        <p className='mb-4'>
+          Indo Russian IT Association <span>(IRITA) </span> is a prestigious forum dedicated to fostering intellectual and technological exchange between India and Russia. Our core mission is to enhance the IT sectors of both nations by <span>bringing together leaders and experts</span> from various domains within the industry. Through collaborative initiatives, <span>knowledge-sharing sessions</span>, and strategic partnerships, IRITA endeavors to propel innovation, drive growth, and foster synergy between the IT ecosystems of India and Russia.
+        </p>
+        <p className="mb-4">
+          INDO-Russian IT Association (IRITA) is a forum for intellectual and technological exchange. IRITA focuses on bettering the IT sector of India and Russia while bringing the leaders & people of the respective departments together.
+        </p>
+        <div className="text-center mb-4">
+          <img src={aboutImg} alt='' className="img-fluid" />
+        </div>
+        <p className="mb-4">
+          Indo Russian IT Association <span>(IRITA) </span> is a prestigious forum dedicated to fostering intellectual and technological exchange between India and Russia. Our core mission is to enhance the IT sectors of both nations by <span>bringing together leaders and experts</span> from various domains within the industry. Through collaborative initiatives, <span>knowledge-sharing sessions</span>, and strategic partnerships, IRITA endeavors to propel innovation, drive growth, and foster synergy between the IT ecosystems of India and Russia.
+        </p>
+        <p>
+          INDO-Russian IT Association (IRITA) is a forum for intellectual and technological exchange. IRITA focuses on bettering the IT sector of India and Russia while bringing the leaders & people of the respective departments together.
+        </p>
+
+        <p className="mb-4">
+          Indo Russian IT Association <span>(IRITA) </span> is a prestigious forum dedicated to fostering intellectual and technological exchange between India and Russia. Our core mission is to enhance the IT sectors of both nations by <span>bringing together leaders and experts</span> from various domains within the industry. Through collaborative initiatives, <span>knowledge-sharing sessions</span>, and strategic partnerships, IRITA endeavors to propel innovation, drive growth, and foster synergy between the IT ecosystems of India and Russia.
+        </p>
+        <p>
+          INDO-Russian IT Association (IRITA) is a forum for intellectual and technological exchange. IRITA focuses on bettering the IT sector of India and Russia while bringing the leaders & people of the respective departments together.
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import '../pages/Membership.css'
 
 export const Membership = () => {
   return (
-    <div>Membership</div>
+    <div>
+      <div className='container mt-5 member'>
+        <h1 className='mb-4'>Membership For Everyone</h1>
+        <p className='mb-4'>All Lions clubs share a single mission: we serve. Yet, no two Lions are exactly alike. That’s what makes our service so strong. The same can be said of our clubs—no two clubs are exactly alike. We offer a variety of club models and membership types to meet the needs of all those looking to serve. Here, we have a membership that’s right for you.</p>
+      </div>
+
+      {/* <div>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <div>
+          <div>
+            <h3>Corporate Membership</h3>
+            <p>Most Lions clubs are formed on the traditional club model.These clubs bring together a group of service-minded individuals who want to better their community, and meet locally on a regular basis. Learn more about traditional clubs.</p>
+          </div>
+          <div>
+            <h3>Corporate Membership Benefits</h3>
+            <div>
+              <ul>
+                <li>Networking opportunities  with industry leaders and government officials.</li>
+                <li>Business promotion through the association's platforms</li>
+                <li>Access to industry reports, webinars, and training programs. Government liaison for advocacy and updates.</li>
+                <li>Brand visibility and representation in industry committees. Exclusive events, discounts, and offers.</li>
+                <li>Membership Fee: ₹50,000 with a Yearly Renewal Fee of ₹25,000.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
   )
 }
