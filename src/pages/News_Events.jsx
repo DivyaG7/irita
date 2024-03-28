c3911de869c1fb0436004e738127bf6c622d08e8
unknown <divyaneela75@gmail.com>
2024-03-28 17:19:32 +0530

    Initial commit

--- src/pages/News_Events.jsx
+++ src/pages/News_Events.jsx
@@ -6,66 +6,67 @@ import { Footer } from '../component/Footer'
 
 export const NewsEvents = () => {
   return (
-    <div className='news-event'>
-      <div className='event-container'>
-        <div className='row'>
-          <div className='col-md-6 bg'>
-            <p id='event-date'>19th August 2021</p>
-            <h1 id='event-h'>President of IRITA</h1>
-            <p id='event-content'>Anirudh Balaji of Horizon Group appointed
-              as the new president of Indo-Russian IT
-              Association.</p>
-          </div>
-          <div className='col-md-6'>
-            <div className='event-img'>
-              <img src={eventImg1} alt='event-img' className="img-fluid" />
-            </div>
-          </div>
-          <div className='col-md-6 third'>
-            <h1>Speeches and published materials</h1>
-            <p>Signed agreements which would take the
-              Kudankulam project forward.
-            </p>
-          </div>
-          <div className='col-md-6 four'>
-            <h1>News and Notices</h1>
-            <p>Signed agreements which would take the
-              Kudankulam project forward.</p>
-          </div>
-          <div className='col-md-6 five pt-4 pb-4'>
-            <div className='hr mb-4'></div>
-            <h5 className='text-white'>STATEGY</h5>
-            <p className='text-white'>FOR A FREE PEACEFULL</p>
-            <p className='text-white'>AND PROSPEROUS</p>
-            <h5 className='text-white'>INDIAN PACIFIC</h5>
-            <h5 className='text-white'>REGION</h5>
-            <div className='hr mt-4'></div>
-          </div>
-          <div className='col-md-6 six p-4'>
-            <h1 className='mb-3'>Press Release</h1>
-            <div className='press'>
-              <Link to='/Speeches' className='link'>Anirudh Balaji of Horizon Group appointed
+    <>
+      <div className='news-event'>
+        <div className='event-container'>
+          <div className='row'>
+            <div className='col-md-6 bg'>
+              <p id='event-date'>19th August 2021</p>
+              <h1 id='event-h'>President of IRITA</h1>
+              <p id='event-content'>Anirudh Balaji of Horizon Group appointed
                 as the new president of Indo-Russian IT
-                Association</Link>
-              <p>at the Kremlin. On the Russian side, the meeting was attended by
-                Foreign Minister Sergei Lavrov, Deputy Prime Minister and Chair of the
-                Russian side </p>
+                Association.</p>
             </div>
-            <div className='hr'></div>
-            <div className='press'>
-              <Link to='/' className='link'>Russian President Vladimir Putin met with
-                Minister of External Affairs of India Dr S
-                Jaishankar </Link>
-              <p>
-                at the Kremlin. On the Russian side, the meeting was attended by
-                Foreign Minister Sergei Lavrov, Deputy Prime Minister and Chair of the
-                Russian side </p>
+            <div className='col-md-6'>
+              <div className='event-img'>
+                <img src={eventImg1} alt='event-img' className="img-fluid" />
+              </div>
+            </div>
+            <div className='col-md-6 third'>
+              <h1>Speeches and published materials</h1>
+              <p>Signed agreements which would take the
+                Kudankulam project forward.
+              </p>
+            </div>
+            <div className='col-md-6 four'>
+              <h1>News and Notices</h1>
+              <p>Signed agreements which would take the
+                Kudankulam project forward.</p>
+            </div>
+            <div className='col-md-6 five pt-4 pb-4'>
+              <div className='hr mb-4'></div>
+              <h5 className='text-white'>STATEGY</h5>
+              <p className='text-white'>FOR A FREE PEACEFULL</p>
+              <p className='text-white'>AND PROSPEROUS</p>
+              <h5 className='text-white'>INDIAN PACIFIC</h5>
+              <h5 className='text-white'>REGION</h5>
+              <div className='hr mt-4'></div>
+            </div>
+            <div className='col-md-6 six p-4'>
+              <h1 className='mb-3'>Press Release</h1>
+              <div className='press'>
+                <Link to='/Speeches' className='link'>Anirudh Balaji of Horizon Group appointed
+                  as the new president of Indo-Russian IT
+                  Association</Link>
+                <p>at the Kremlin. On the Russian side, the meeting was attended by
+                  Foreign Minister Sergei Lavrov, Deputy Prime Minister and Chair of the
+                  Russian side </p>
+              </div>
+              <div className='hr'></div>
+              <div className='press'>
+                <Link to='/' className='link'>Russian President Vladimir Putin met with
+                  Minister of External Affairs of India Dr S
+                  Jaishankar </Link>
+                <p>
+                  at the Kremlin. On the Russian side, the meeting was attended by
+                  Foreign Minister Sergei Lavrov, Deputy Prime Minister and Chair of the
+                  Russian side </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <Footer />
-    </div>
-
+    </>
   )
 }
--- src/pages/Programmes.jsx
+++ src/pages/Programmes.jsx
@@ -10,94 +10,96 @@ import { Footer } from '../component/Footer'
 
 export const Programmes = () => {
     return (
-        <div className='programmes'>
-            <div className='row'>
-                <h2>Programmes</h2>
-                <div className='col-md-8'>
-                    <div className='left-program'>
-                        <p>01 March, 2023</p>
-                        <h5>Workshop on cyber crime against women held</h5>
-                        <div className='row left-content pt-4'>
-                            <div className='col-md-4 pro-img mb-3'>
-                                <img src={program1} alt='img' className="img-fluid" />
-                            </div>
-                            <div className='col-md-8 p-left'>
-                                <p id='first-pro'>Chennai: State minister of information technology T Mano thangaraj inaugrated a
-                                    workshop against on cyber crime against women in Indo Russian IT Association,
-                                    Teynampet.</p>
-                                <p id='second-pro'>Initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='left-program mt-5'>
-                        <p>28 Febrauary , 2023</p>
-                        <h5>Cyber Crime Against Women Summit ‘23</h5>
-                        <div className='row left-content pt-4'>
-                            <div className='col-md-4 pro-img mb-3'>
-                                <img src={program2} alt='img' className="img-fluid" />
-                            </div>
-                            <div className='col-md-8 p-left'>
-                                <p id='first-pro'>The Cyber Crime Against Women Summit ’23 was an event organized by C20 Amrita Vishwa Vidyapeetham on 28th February 2023, in association with the Government of Tamil Nadu and the Tamil Nadu State Commission for Women, an initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association. </p>
-                                <p id='second-pro'>Initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='left-program mt-5 women-event'>
-                        <div className='row left-content pt-4'>
-                            <div className='col-md-4 pro-img mb-3 women-bg'>
-                                <img src={program5} alt='img' className="img-fluid" />
-                            </div>
-                            <div className='col-md-8 p-left'>
-                                <Link to='' id='first-pro' className='women-links'>International Womens day - 2024</Link>
-                                <p id='second-pro'>March - 12 (2024)</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className='col-md-4 pe-4 pb-5'>
-                    <div className='row gap-5'>
-                        <div className='col-md-12 right-program'>
-                            <div className='right-pro'>
-                                <h5 className='ps-3 pt-1 pb-1'>Recent posts</h5>
-                            </div>
-                            <div className='right-pro-content'>
-                                <p className='mt-2'> The U.S. Department of Commerce, International
-                                    Trade Service </p>
-                                <img src={program3} alt='img' className="img-fluid" />
-                                <p className='mt-2'> The U.S. Department of Commerce, International
-                                    Trade Service </p>
-                                <img src={program4} alt='img' className="img-fluid" />
-                            </div>
-                        </div>
-                        <div className='col-md-12 bottom-links'>
-                            <div className='right-links-heading'>
-                                <h5 className='ps-3 pt-1 pb-1'>Quick Links</h5>
-                            </div>
-                            <div className='right-links'>
-                                <div className='pt-1'>
-                                    <Link to='/speeches' className='text-decoration-none links'>Anirudh Balaji of Horizon Group appointed
-                                        as the new president of Indo-Russian IT
-                                        Association</Link>
-                                    <div className="read">
-                                        <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
-                                    </div>
+        <>
+            <div className='programmes'>
+                <div className='row'>
+                    <h2>Programmes</h2>
+                    <div className='col-md-8'>
+                        <div className='left-program'>
+                            <p>01 March, 2023</p>
+                            <h5>Workshop on cyber crime against women held</h5>
+                            <div className='row left-content pt-4'>
+                                <div className='col-md-4 pro-img mb-3'>
+                                    <img src={program1} alt='img' className="img-fluid" />
                                 </div>
-
-                                <div className='pt-3'>
-                                    <Link to='' className='text-decoration-none links'>Russian President Vladimir Putin met with Minister of External Affairs of India Dr S Jaishankar</Link>
-                                    <div className="read">
-                                        <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
-                                    </div>
+                                <div className='col-md-8 p-left'>
+                                    <p id='first-pro'>Chennai: State minister of information technology T Mano thangaraj inaugrated a
+                                        workshop against on cyber crime against women in Indo Russian IT Association,
+                                        Teynampet.</p>
+                                    <p id='second-pro'>Initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association.</p>
                                 </div>
+                            </div>
+                        </div>
 
-                                <div className='pt-3'>
-                                    <Link to='' className='text-decoration-none links'>International Womens day - 2024</Link>
-                                    <div className="read">
-                                        <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
+                        <div className='left-program mt-5'>
+                            <p>28 Febrauary , 2023</p>
+                            <h5>Cyber Crime Against Women Summit ‘23</h5>
+                            <div className='row left-content pt-4'>
+                                <div className='col-md-4 pro-img mb-3'>
+                                    <img src={program2} alt='img' className="img-fluid" />
+                                </div>
+                                <div className='col-md-8 p-left'>
+                                    <p id='first-pro'>The Cyber Crime Against Women Summit ’23 was an event organized by C20 Amrita Vishwa Vidyapeetham on 28th February 2023, in association with the Government of Tamil Nadu and the Tamil Nadu State Commission for Women, an initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association. </p>
+                                    <p id='second-pro'>Initiative by the National Cyber Security Research Council supported by the Indo-Russian IT association.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='left-program mt-5 women-event'>
+                            <div className='row left-content pt-4'>
+                                <div className='col-md-4 pro-img mb-3 women-bg'>
+                                    <img src={program5} alt='img' className="img-fluid" />
+                                </div>
+                                <div className='col-md-8 p-left'>
+                                    <Link to='' id='first-pro' className='women-links'>International Womens day - 2024</Link>
+                                    <p id='second-pro'>March - 12 (2024)</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='col-md-4 pe-4 pb-5'>
+                        <div className='row gap-5'>
+                            <div className='col-md-12 right-program'>
+                                <div className='right-pro'>
+                                    <h5 className='ps-3 pt-1 pb-1'>Recent posts</h5>
+                                </div>
+                                <div className='right-pro-content'>
+                                    <p className='mt-2'> The U.S. Department of Commerce, International
+                                        Trade Service </p>
+                                    <img src={program3} alt='img' className="img-fluid" />
+                                    <p className='mt-2'> The U.S. Department of Commerce, International
+                                        Trade Service </p>
+                                    <img src={program4} alt='img' className="img-fluid" />
+                                </div>
+                            </div>
+                            <div className='col-md-12 bottom-links'>
+                                <div className='right-links-heading'>
+                                    <h5 className='ps-3 pt-1 pb-1'>Quick Links</h5>
+                                </div>
+                                <div className='right-links'>
+                                    <div className='pt-1'>
+                                        <Link to='/speeches' className='text-decoration-none links'>Anirudh Balaji of Horizon Group appointed
+                                            as the new president of Indo-Russian IT
+                                            Association</Link>
+                                        <div className="read">
+                                            <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
+                                        </div>
+                                    </div>
+
+                                    <div className='pt-3'>
+                                        <Link to='' className='text-decoration-none links'>Russian President Vladimir Putin met with Minister of External Affairs of India Dr S Jaishankar</Link>
+                                        <div className="read">
+                                            <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
+                                        </div>
+                                    </div>
+
+                                    <div className='pt-3'>
+                                        <Link to='' className='text-decoration-none links'>International Womens day - 2024</Link>
+                                        <div className="read">
+                                            <Link className="text-decoration-none links" to=''><span>&rarr;</span>(read...)</Link>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -105,9 +107,7 @@ export const Programmes = () => {
                     </div>
                 </div>
             </div>
-
             <Footer />
-
-        </div>
+        </>
     )
 }
