170144377b49af4c5cd262bf9d84c6f77cc36467
unknown <divyaneela75@gmail.com>
2024-05-10 15:19:08 +0530

    Initial commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,15 +5,19 @@ export const Footer = () => {
         <footer>
             <div className='footer'>
                 <div className='footer-content'>
+                    <div className='d-flex'>
                     <p>Copyright policy</p>
                     <span className='vertical-line'></span>
                     <p>Terms and condition</p>
                     <span className='vertical-line'></span>
+                    </div>
+                    <div className='d-flex'>
                     <p>Privacy Policy</p>
                     <span className='vertical-line'></span>
                     <p>Help </p>
                     <span className='vertical-line'></span>
                     <p>Contact Us</p>
+                    </div>
                 </div>
             </div>
         </footer>
